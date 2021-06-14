20933bc6dbf1cc814800353f043d8fbf2e595d14
Andrei Sukhanov <sukhanov@bnl.gov>
2021-06-14 00:35:40 -0400

    multiple parameters per device

--- apstrim/__main__.py
+++ apstrim/__main__.py
@@ -5,7 +5,7 @@ import sys, argparse
 from .apstrim  import apstrim 
 
 def main():
-	
+    
     # parse common arguments
     parser = argparse.ArgumentParser(description=Description)
     parser.add_argument('-c', '--compress', action='store_true', help=\
@@ -18,9 +18,10 @@ def main():
     'File for storing PVs and data objects')
     parser.add_argument('-n', '--namespace', default='EPICS', help=\
     'Infrastructure namespace, e.g.: EPICS, ADO or LITE')
-    parser.add_argument('pvNames', nargs='*') 
+    parser.add_argument('pvNames', nargs='*', help=\
+    'Data Object names, one item per device parameters are comma-separated: dev1:par1,par2 dev2:par1,par2') 
     pargs = parser.parse_args()
-    print(f'pargs:{pargs}')
+    #print(f'pargs:{pargs}')
 
     s = pargs.namespace.upper()
     if s == 'LITE':
@@ -32,7 +33,18 @@ def main():
         print(f'ERROR: Unsupported namespace {s}')
         sys.exit(1)
 
-    ups = apstrim(pargs.outfile, publisher, pargs.pvNames, pargs.compress) 
+    pvNames = []
+    for pvn in pargs.pvNames:
+        tokens = pvn.split(',')
+        first = tokens[0]
+        pvNames.append(first)
+        if len(tokens) > 1:
+            dev = first.rsplit(':',1)[0]
+            for par in tokens[1:]:
+                pvNames.append(dev+':'+par)
+    #print(f'pvNames: {pvNames}')
+
+    ups = apstrim(pargs.outfile, publisher, pvNames, pargs.compress) 
 
 if __name__ == '__main__':
     main()
